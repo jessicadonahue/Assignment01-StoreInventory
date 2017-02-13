323d58596951afe70aaef4080ec386cc86858e90
jessicadonahue <jmd732@nyu.edu>
2017-02-13 11:29:33 -0500

    Initial Commit

---
+++ Store/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Store
+//
+//  Created by Jessica Donahue on 2/13/17.
+//  Copyright © 2017 nyu.edu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
